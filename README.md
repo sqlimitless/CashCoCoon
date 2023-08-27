dc005011257c0f0a45b6724ed56c9cb90cb36c64
sqlimitless <lhjhoon@gmail.com>
2023-08-27 13:52:38 +0900

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# CashCoCoon
+
+## 가계부
+"Cash"의 현금, "Cocoon"의 번데기의 합성어로 재무를 안전하게 보호하고 성장시킨다는 의미의 가계부 서비스
+
+### 스킬
+Java 17, SpringBoot 3, Jpa, SpringSecurity, H2
---
+++ src/main/java/com/hoon/cashcocoon/CashCocoonApplication.java
@@ -0,0 +1,13 @@
+package com.hoon.cashcocoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashCocoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashCocoonApplication.class, args);
+    }
+
+}
